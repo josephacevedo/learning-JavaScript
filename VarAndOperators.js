474889377c3ed0ec9dab059036b0c86ff92ce4b7
Joseph Acevedo <josephacevedo1997@gmail.com>
2019-03-18 12:07:43 -0500

    Add files via upload
    learning to use console.log and variables

---
+++ VarAndOperators.js
@@ -0,0 +1,14 @@
+let name = "Joseph";
+let address = "123 abc Drive"
+let age = 22;
+let birthday = "march 8";
+
+console.log("name: " + name + "\nage: " + age + "\nAddress: " + address + "\nbirthday: " + birthday  );
+console.log("age in day is:" +(age*365));
+console.log("age in seconds is:"+ (age*365*24*60));
+console.log("there are " + (365*24*60) + " seconds in a year");
+var num = 365;
+num *= 24;
+num *= 60;
+console.log("seconds in a year: " + num);
+
