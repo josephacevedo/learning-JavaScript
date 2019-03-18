0d79369d739d1c7fa5ffb843b8024304d4beeed9
Joseph Acevedo <josephacevedo1997@gmail.com>
2019-03-18 12:10:22 -0500

    Add files via upload
    learning to use variables

---
+++ Lab1.js
@@ -0,0 +1,23 @@
+let name = "edge tech academy";
+let dayOfWeek = "Wednesday";
+let favAuthor = "Dr. Seuss";
+let favNumber = 137;
+let currentHour = 11;
+let roomNumber = 610;
+console.log("My favorite number is:" + favNumber);
+console.log("I like to read " + favAuthor + " at " + currentHour + " o'clock on " + dayOfWeek + "s in room # " + roomNumber);
+console.log(roomNumber + favNumber + currentHour);
+
+let allVars = name + dayOfWeek + favAuthor + favNumber + currentHour + roomNumber;
+console.log(allVars);
+
+let newVars = roomNumber + currentHour + favNumber + favAuthor + dayOfWeek + name;
+console.log(newVars);
+
+favNumber +=7;
+favNumber *= 13;
+favNumber /= 6;
+
+console.log(favNumber);
+
+
