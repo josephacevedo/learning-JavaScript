a4eac2f05a0feba87dd2a3af426f80ad82a3ab6e
Joseph Acevedo <josephacevedo1997@gmail.com>
2019-03-19 11:09:40 -0500

    Add files via upload
    A practice of me using strings

---
+++ Strings.js
@@ -0,0 +1,23 @@
+
+// Original string
+let str = "We hold these truths to be self-evident, taht all men are created equal, taht they are endowed by their Creator with certain unalienable Rights, taht among these are Life, Liberty and the pursuit of Happiness. Taht to secure these rights, Governments are instituted among Men, deriving their just powers from the consent of the governed.";
+
+// 2) index of the word Governments
+let indxOf = str.indexOf("Governments")
+console.log("The index of the word Governments is :" + indxOf);
+
+// index of the word all
+let indxOfall = str.indexOf("all")
+console.log("The index of the word all:" + indxOfall);
+
+// 3) substring from all to next comma
+let substr = str.substring(46,71);
+console.log(substr);
+
+// 4) replace taht with that
+let replace = str.replace("taht","that");
+console.log(replace);
+
+// 5) length of string
+let length = str.length;
+console.log(length);
